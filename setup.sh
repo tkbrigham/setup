637c845e6b1ee01c3714b52c0ef1d7325a5e0291
Thomas Brigham <tkbrigham@Thomass-MacBook-Pro.local>
2015-09-13 15:15:30 -0700

    Updates

--- setup.sh
+++ setup.sh
@@ -1,8 +1,28 @@
-### Set up colors and setup directory
+#####
+### FUNCTIONS
+#####
+
+### Set color printing functions and current dir
 printfBlue() { printf "\e[34m$1" }
 printfGreen() { printf "\e[32m$1" }
+printfRed() { printf "\e[31m$1" }
 resetColor() { printf "\e[0m" }
 setup_dir=$(pwd)
+
+### Run a command silently, checking for success
+silent_check() {
+	printfBlue "  # Running $1..."
+	if ($1 > /dev/null 2>&1); then
+		printfGreen "DONE\n"
+	else
+		printfGreen "already installed\n"
+	fi
+	resetColor
+}
+
+#####
+### APPLE DEFAULTS
+#####
 
 # Change key repeat speed
 defaults write -g KeyRepeat -int 1
@@ -11,13 +31,17 @@ defaults write -g InitialKeyRepeat -int 15
 # Disable character accent selector on key hold (most common on vowels)
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+#####
+### ZSHELL
+#####
+
 # Make zshell your shell always
 printfBlue '>> Setting shell to zshell...'
 if [ 'echo $0'='-zsh' ]; then
-	printfBlue 'already set\n'
+	printfGreen 'already set\n\n'
 else
 	chsh -s /bin/zsh
-	printfGreen 'done'
+	printfGreen 'DONE'
 fi
 resetColor
 
@@ -25,11 +49,32 @@ resetColor
 printfBlue ">> Installing xcode-select..."
 if ! type "xcode-select" > /dev/null; then 
 	xcode-select --install
-	printfGreen "...done\n"
+	printfGreen "...DONE\n\n"
 else
-	printfBlue "already installed\n"
+	printfGreen "already installed\n\n"
 fi
 resetColor
+
+#####
+### Prezto
+#####
+
+# Install pretzo, a replacement for oh-my-zsh
+printfBlue ">> Installing ~/.zpretzo..."
+if [ -d ~/.zprezto ]; then
+	printfGreen "already exists\n\n"
+else
+	resetColor
+	git clone --quiet --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+	cd $setup_dir
+	zsh run/prezto_config.sh
+	printfGreen "...DONE\n\n"
+fi
+resetColor
+
+#####
+### VIM 
+#####
 
 # Install pathogen
 printfBlue ">> Installing pathogen..."
@@ -37,51 +82,52 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 if ! grep -q 'execute pathogen#infect()' ~/.vimrc; then
 	printf "execute pathogen#infect()" >> ~/.vimrc
-	printfGreen "...done\n"
+	printfGreen "...DONE\n\n"
 else
-	printfBlue "already set up\n"
+	printfGreen "already set up\n\n"
 fi
 resetColor
 
-# Add Tim Pope's super minimal vim settings from github.com/tpope/vim-pathogen
-printfBlue ">> Installing Tim Pope's minimal vim settings..."
-if ! grep -q 'filetype plugin indent on' ~/.vimrc; then
-	printf "\nsyntax on\nfiletype plugin indent on" >> ~/.vimrc
-	printfGreen "...done\n"
-else
-	printfBlue "already installed\n"
-fi
-resetColor
+### Move to vim/bundle for pathogen install of plugins
+cd ~/.vim/bundle
 
-# Add vim-sensible
-printfBlue ">> Installing vim-sensible..."
-if cd ~/.vim/bundle/vim-sensible; then
-	printfBlue "already installed\n"
-else
-	cd ~/.vim/bundle/vim-sensible
-	git clone git://github.com/tpope/vim-sensible.git
-	cd ~/code
-	printfGreen "...done\n"
-fi
-resetColor
+### install vim-snipmate: https://github.com/garbas/vim-snipmate
+printfBlue ">> Installing vim-snipmate...\n"
+printfBlue "-------------\n"
+silent_check "git clone https://github.com/tomtom/tlib_vim.git"
+silent_check "git clone https://github.com/MarcWeber/vim-addon-mw-utils.git"
+silent_check "git clone https://github.com/garbas/vim-snipmate.git"
+silent_check "git clone https://github.com/honza/vim-snippets.git"
+printfBlue "-------------\n"
+printfGreen "DONE\n\n"
 
-### TODO: ADD OTHER .VIMRC THINGS HERE
 
-# Install pretzo, a replacement for oh-my-zsh
-printfBlue ">> Installing ~/.zpretzo..."
-if [ -d ~/.zprezto ]; then
-	printfBlue "already exists\n"
-else
-	resetColor
-	echo ""
-	git clone --quiet --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-	cd $setup_dir
-	zsh run/prezto_config.sh
-	printfGreen "...done\n"
-fi
-resetColor
+
+## Vetted:
+# 1. SnipMate - Ruby, HAML, javascript, CSS, vim, zsh, Go?
+# 2. BufExplorer (lanzarotta)
+# 3. NERDTREE (grenfell)
+# 4. Easy Motion
+# 5. Surround
+# 6. Matchit (benji fischer)
+# 7. Fugitive
+# 8. vim rails
+# 9. vim rubocop
+# 10. vim syntastic
+# 11. vim speeddating
+# 12. powerline
+# 13. Ctrl-P
+# set <leader> to single space
+# vim-mkdir
+# solarized - iterm and vim
+# supertab (ervandrew)
+
+
 
 ### TODO: UPDATE PRETZO SETTINGS FOR SHELL NAV
+# shell https://github.com/davidjrice/prezto_powerline
+
+### TODO: tmux??
 
 ### TODO: ITERM2 PREFERENCES - SET, SAVE, AND LOAD INTO FOLDER
 
@@ -89,8 +135,5 @@ resetColor
 
 ### TODO: github keys??
 
-
-
-
-
 ## MUST RESTART FOR KEYSTROKE REPEAT TO WORK
+
