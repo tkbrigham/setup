c33905cbb3a450d262be19524539a173e1871496
Thomas Brigham <tkbrigham@Thomass-MacBook-Pro.local>
2015-09-15 18:47:10 -0700

    Finalize setup script

--- run/prezto_config.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/zsh
-setopt extended_glob
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
--- setup.sh
+++ setup.sh
@@ -1,180 +1,170 @@
-### If anything fails, let the console know
-trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
-
-#####
-### FUNCTIONS
-#####
-
-### Set color printing functions and current dir
-printfBlue2() { printf "\e[36m$1" }
-printfBlue() { printf "\e[34m$1" }
-printfGreen() { printf "\e[32m$1" }
-printfRed() { printf "\e[31m$1" }
-resetColor() { printf "\e[0m" }
-setup_dir=$(pwd)
-
-### Run a command silently, checking for success
-silent_check() {
-	eval $1 > /dev/null 2>&1
-	resetColor
-}
-
-quiet_check() {
-	eval $1 > /dev/null 2>&1
-	ret=$?
-	if [ $ret = 0 ]; then
-		printfGreen "DONE\n"
-	else
-		printfGreen "already installed\n"
-	fi
-	resetColor
-}
-
-noisy_check() {
-	printfBlue2 "    # "
-	printfBlue "Running $1..."
-	eval $1 > /dev/null 2>&1
-	ret=$?
-	if [ $ret = 0 ]; then
-		printfGreen "DONE\n"
-	else
-		printfGreen "already installed\n"
-	fi
-	resetColor
-}
-
-### Change key repeat speed
+cd ~
+###
+# Apple defaults
+###
+# Change key repeat speed
 defaults write -g KeyRepeat -int 1
-defaults write -g InitialKeyRepeat -int 15
+defaults write -g InitialKeyRepeat -int 12
 
-### Disable character accent selector on key hold (most common on vowels)
+# Disable character accent selector on key hold (most common on vowels)
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-### Make zshell your shell always
-printfBlue '>> Setting shell to zshell...'
-if [ 'echo $0'='-zsh' ]; then
-	printfGreen 'already set\n\n'
-else
-	chsh -s /bin/zsh
-	printfGreen 'DONE'
-fi
-resetColor
+# Set alert volume to 0
+osascript -e "set volume alert volume 0"
+###
 
-### Install xcode-select
-printfBlue ">> Installing xcode-select..."
-if ! type "xcode-select" > /dev/null; then 
-	xcode-select --install
-	printfGreen "...DONE\n\n"
-else
-	printfGreen "already installed\n\n"
-fi
-resetColor
+# zshell
+chsh -s $(which zsh) > /dev/null 2>&1
 
-### Install Homebrew
-printfBlue "Installing Homebrew...\n"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install xcode
+xcode-select --install > /dev/null 2>&1 || echo "Xcode already installed"
 
-### thoughtbot dotfiles
-printfBlue "Installing thoughtbot dotfiles...\n"
-cd ~
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2>&1 || echo "Homebrew already installed"
+
+# Thoughtbot's dotfiles
 git clone git://github.com/thoughtbot/dotfiles.git
 brew tap thoughtbot/formulae
 brew install rcm
-env RCRC=$HOME/DOTFILES/rcrc rcup
+env RCRC=$HOME/dotfiles/rcrc rcup
+rcup
 
-#####
-### Prezto
-#####
+# Install rbenv
+brew update
+brew install rbenv ruby-build
 
-# Install pretzo, a replacement for oh-my-zsh
-printfBlue ">> Installing ~/.zpretzo..."
-if [ -d ~/.zprezto ]; then
-	printfGreen "already exists\n\n"
+# Powerline for zshell
+if ! grep -q "_update_ps1()" ~/.zshrc; then
+cat <<EOT >> ~/.zshrc
+
+# https://github.com/carlcarl/powerline-zsh
+export TERM="xterm-256color"
+function _update_ps1() {
+  export PROMPT="\$(~/powerline-zsh.py \$?)"
+}
+precmd()
+{
+  _update_ps1
+}
+EOT
+  echo "Powerline for Zsh added"
 else
-	resetColor
-	git clone --quiet --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-	cd $setup_dir
-	zsh run/prezto_config.sh
-	printfGreen "...DONE\n\n"
+  echo "Powerline for Zsh already present"
 fi
-resetColor
 
-#####
-### VIM 
-#####
+# Persistent undo in vim
+if [ ! -d ~/.vim/undodir ]; then
+  mkdir ~/.vim/undodir
+cat <<EOT >> ~/.vimrc
 
-printfBlue2 "##########\n"
-printfBlue2 "###### VIM CONFIGS\n"
-printfBlue2 "##########\n\n"
-
-# Install pathogen
-printfBlue ">> Installing pathogen..."
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-if ! grep -q 'execute pathogen#infect()' ~/.vimrc; then
-	printf "execute pathogen#infect()" >> ~/.vimrc
-	printfGreen "DONE\n\n"
+set undodir=~/.vim/undodir
+set undofile
+set undolevels=1000
+set undoreload=10000
+EOT
+  echo "Set up persistent undo in vim"
 else
-	printfGreen "already set up\n\n"
+  echo 'undodir already in .vimrc'
 fi
-resetColor
 
-### Move to vim/bundle for pathogen install of plugins
+# Powerline for vim
+brew install python
+pip install https://github.com/Lokaltog/powerline/tarball/develop
+
+if ! grep -q "plugin/powerline.vim" ~/.vimrc; then
+cat <<EOT >> ~/.vimrc
+source /usr/local/lib/python2.7/site-packages/powerline/bindings/vim/plugin/powerline.vim
+set laststatus=2
+EOT
+  echo "powerline.vim installed"
+else
+  echo "powerline.vim already installed"
+fi
+
+## Setup Iterm2 > Preferences > Profiles > Text to have font 14pt Source Code Pro medium, vertical 1.0 and horizontal 0.8
+echo "CHECK LINE 77"
+
+# ~/.tmux.conf.local
+brew install reattach-to-user-namespace
+if [ ! -e ~/.tmux.conf.local ]; then
+cat <<EOT >> ~/.tmux.conf.local
+### from https://robots.thoughtbot.com/tmux-copy-paste-on-os-x-a-better-future
+setw -g mode-keys vi
+
+# Setup 'v' to begin selection as in Vim
+bind-key -t vi-copy v begin-selection
+bind-key -t vi-copy y copy-pipe "reattach-to-user-namespace pbcopy"
+
+# Update default binding of `Enter` to also use copy-pipe
+unbind -t vi-copy Enter
+bind-key -t vi-copy Enter copy-pipe "reattach-to-user-namespace pbcopy"
+
+# New window with default path set to last path
+bind '"' split-window -c "#{pane_current_path}"
+bind % split-window -h -c "#{pane_current_path}"
+bind c new-window -c "#{pane_current_path}"
+EOT
+  echo "Installed ~/.tmux.conf.local lines"
+else
+  echo "~/.tmux.conf.local already present"
+fi
+
+# vim y and p between iterm2 sessions (after restart)
+if ! grep -q 'clipboard+=unnamed' ~/.vimrc; then
+  echo "set clipboard+=unnamed" >> ~/.vimrc
+  echo "clipboard=unnamed set in .vimrc"
+else
+  echo "clipboard=unnamed already set in .vimrc"
+fi
+
+# cd into .vim/bundle for vim installs
 cd ~/.vim/bundle
 
-### install vim-snipmate: https://github.com/garbas/vim-snipmate
-printfBlue ">> Installing vim-snipmate...\n"
-noisy_check "git clone https://github.com/tomtom/tlib_vim.git"
-noisy_check "git clone https://github.com/MarcWeber/vim-addon-mw-utils.git"
-noisy_check "git clone https://github.com/garbas/vim-snipmate.git"
-noisy_check "git clone https://github.com/honza/vim-snippets.git"
+echo "current dir is $(pwd)"
 
-printfBlue "\n>> Installing bufexplorer..."
-quiet_check "git clone https://github.com/jlanzarotta/bufexplorer"
+# Install vim-snippets
+if [ ! -d ~/.vim/bundle/vim-snippets ]; then
+  echo "VIM SNIPPETS NOT FOUND"
+  git clone https://github.com/tomtom/tlib_vim.git
+  git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+  git clone https://github.com/garbas/vim-snipmate.git
+  git clone https://github.com/honza/vim-snippets.git
+  echo "vim-snippets files installed"
+else
+  echo "vim-snippets files already installed"
+fi
 
-printfBlue "\n>> Installing NERDtree..."
-quiet_check "git clone https://github.com/scrooloose/nerdtree.git"
-# TODO: nerdtree-git-plugin not installed
+# Install nerdtree
+if [ ! -d ~/.vim/bundle/nerdtree ]; then
+  echo "VIM NERDTREE NOT FOUND"
+  git clone https://github.com/scrooloose/nerdtree.git
+  echo "installed nerdtree"
+else
+  echo "nerdtree already installed"
+fi
 
-printfBlue "\n>> Installing vim-repeat..."
-quiet_check "https://github.com/tpope/vim-repeat"
+# Install vim-surround
+if [ ! -d ~/.vim/bundle/vim-surround ]; then
+  echo "VIM-SURROUND NOT FOUND"
+  git clone git://github.com/tpope/vim-surround.git
+  echo "installed vim-surround"
+else
+  echo "vim-surround already installed"
+fi
 
-printfBlue "\n>> Installing vim-easymotion..."
-quiet_check "git clone https://github.com/easymotion/vim-easymotion"
-# TODO: tutorial at http://code.tutsplus.com/tutorials/vim-essential-plugin-easymotion--net-19223
+# Install vim-go
+if [ ! -d ~/.vim/bundle/vim-go ]; then
+  echo "VIM-GO NOT FOUND"
+  git clone https://github.com/fatih/vim-go.git
+  echo "installed vim-go"
+else
+  echo "vim-go already installed"
+fi
 
-printfBlue "\n>> Installing vim-surround..."
-quiet_check "git clone git://github.com/tpope/vim-surround.git"
-
-printfBlue "\n>> Installing vim-fugitive..."
-quiet_check "git clone git://github.com/tpope/vim-fugitive.git"
-vim -u NONE -c \"helptags vim-fugitive/doc\" -c q
-
-printfBlue "\n>> Installing vim-rails..."
-silent_check "git clone git://github.com/tpope/vim-rails.git"
-quiet_check "git clone git://github.com/tpope/vim-bundler.git"
-
-# 10. vim rubocop
-# 11. vim speeddating
-# 12. powerline
-# 13. Ctrl-P
-# set <leader> to single space
-# vim-mkdir
-# solarized - iterm and vim
-# supertab (ervandrew)
-
-
-
-### TODO: UPDATE PRETZO SETTINGS FOR SHELL NAV
-# shell https://github.com/davidjrice/prezto_powerline
-
-### TODO: tmux??
-
-### TODO: ITERM2 PREFERENCES - SET, SAVE, AND LOAD INTO FOLDER
-
-### TODO: RESTART TERMINAL
-
-### TODO: github keys??
-
-## MUST RESTART FOR KEYSTROKE REPEAT TO WORK
-
+vared -p "Restart comp (for settings)? (yn): " -c restart
+if [ $restart = 'y' ]; then
+  printf "Enter password to reboot...\n"
+  sudo reboot
+else
+  echo "Not rebooting"
+fi
